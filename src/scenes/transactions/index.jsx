4dac899d8f454ba5d01b881d08e624546bbb7ba8
theblackman <paskasonga@gmail.com>
2025-02-01 18:56:56 +0100

    feat: dsiplay transactions dates

--- src/scenes/transactions/index.jsx
+++ src/scenes/transactions/index.jsx
@@ -1,108 +1,95 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { useGetTransactionsQuery } from "../../state/api";
 import Header from "../../components/Header";
-import DataGridCustomToolbar from "../../components/DataGridCustomToolbar";
 import axiosInstance from "../../services/axios";
 
 const Transactions = () => {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  // values to be sent to the backend
-  const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(20);
-  const [sort, setSort] = useState({});
-  const [search, setSearch] = useState("");
-  const [transactions, setTransactions] =  useState([]);
-  const [searchInput, setSearchInput] = useState("");
-  const [loading, setLoading] = useState(false)
+	// values to be sent to the backend
+	const [transactions, setTransactions] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    fetchTransactions();
-    
-  }, [])
-  const fetchTransactions = async () => {
-    setLoading(true)
-    try {
-      const { data } = await axiosInstance('/transactions?nbr=1000&all=1')
-      setTransactions(data)
-    } catch (e) {
+	useEffect(() => {
+		fetchTransactions();
+	}, []);
+	const fetchTransactions = async () => {
+		setLoading(true);
+		try {
+			const { data } = await axiosInstance("/transactions?nbr=1000&all=1");
+			setTransactions(data);
+		} catch (e) {
+		} finally {
+			setLoading(false);
+		}
+	};
 
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  return (
-    <Box m="1.5rem 2.5rem">
-      <Header title="TRANSACTIONS"  />
-      <Box
-        height="80vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: theme.palette.background.alt,
-            color: theme.palette.secondary[100],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: theme.palette.primary.light,
-          },
-          "& .MuiDataGrid-footerContainer": {
-            backgroundColor: theme.palette.background.alt,
-            color: theme.palette.secondary[100],
-            borderTop: "none",
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${theme.palette.secondary[200]} !important`,
-          },
-        }}
-      >
-        {
-          loading ? <div>Loading....</div> : (
-            <div>
-              <div className="row col-4 header">
-                      
-                  <span className="column">Nom</span>
-                  <span className="column">Balance</span>
-                  <span className="column">Types</span>                              
-                  <span className="column">Satus</span>
-                
-              </div>
-              <div>
-                {
-                   transactions.map((u) => {
-                    return (
-                      <div className="row col-4" key={u.uid}>
-                        <span className="column" >
-                        {u.user?.displayName}
-                        </span >
-                        <span className="column">
-                          {u.amount ?? 0}
-                        </span >
-                        <span className="column">
-                          {u.type ?? 0}
-                        </span >
-                        <span className="column">
-                          { u.status}
-                        </span >
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            </div>
-          )
-        }
-      </Box>
-    </Box>
-  );
+	return (
+		<Box m="1.5rem 2.5rem">
+			<Header title="TRANSACTIONS" />
+			<Box
+				height="80vh"
+				sx={{
+					"& .MuiDataGrid-root": {
+						border: "none",
+					},
+					"& .MuiDataGrid-cell": {
+						borderBottom: "none",
+					},
+					"& .MuiDataGrid-columnHeaders": {
+						backgroundColor: theme.palette.background.alt,
+						color: theme.palette.secondary[100],
+						borderBottom: "none",
+					},
+					"& .MuiDataGrid-virtualScroller": {
+						backgroundColor: theme.palette.primary.light,
+					},
+					"& .MuiDataGrid-footerContainer": {
+						backgroundColor: theme.palette.background.alt,
+						color: theme.palette.secondary[100],
+						borderTop: "none",
+					},
+					"& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+						color: `${theme.palette.secondary[200]} !important`,
+					},
+				}}
+			>
+				{loading ? (
+					<div>Loading....</div>
+				) : (
+					<div>
+						<div className="grid grid-cols-5 header">
+							<span className="column">Nom</span>
+							<span className="column">Balance</span>
+							<span className="column">Date</span>
+							<span className="column">Types</span>
+							<span className="column">Satus</span>
+						</div>
+						<div>
+							{[...transactions]
+								.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+								.map((u) => {
+									return (
+										<div className="row grid grid-cols-5" key={u.uid}>
+											<span className="column">{u.user?.displayName}</span>
+											<span className="column">{u.amount ?? 0}</span>
+											<span className="column">
+												{Intl.DateTimeFormat("fr", {
+													dateStyle: "long",
+													timeStyle: "medium",
+												}).format(new Date(u.createdAt))}
+											</span>
+											<span className="column">{u.type ?? 0}</span>
+											<span className="column">{u.status}</span>
+										</div>
+									);
+								})}
+						</div>
+					</div>
+				)}
+			</Box>
+		</Box>
+	);
 };
 
 export default Transactions;
--- src/scenes/users/index.jsx
+++ src/scenes/users/index.jsx
@@ -1,109 +1,99 @@
 import React, { useEffect, useState } from "react";
 import { Box, useTheme } from "@mui/material";
-import { useGetCustomersQuery } from "../../state/api";
 import Header from "../../components/Header";
-import { DataGrid } from "@mui/x-data-grid";
 import axiosInstance from "../../services/axios";
 
 const Users = () => {
-  const theme = useTheme();
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(false)
+	const theme = useTheme();
+	const [users, setUsers] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
+	useEffect(() => {
+		fetchUsers();
+	}, []);
 
-  const fetchUsers = async () => {
-    setLoading(true)
-    try {
-      const { data } = await axiosInstance('/users')
-      setUsers(data)
-    } catch (e) {
+	const fetchUsers = async () => {
+		setLoading(true);
+		try {
+			const { data } = await axiosInstance("/users");
+			setUsers(data);
+		} catch (e) {
+		} finally {
+			setLoading(false);
+		}
+	};
 
-    } finally {
-      setLoading(false)
-    }
-  }
+	return (
+		<Box m="1.5rem 2.5rem">
+			<Header
+				title="Users"
+				subtitle={`Liste des utilisateurs: ${users.length}`}
+			/>
+			<Box
+				mt="40px"
+				height="75vh"
+				sx={{
+					"& .MuiDataGrid-root": {
+						border: "none",
+					},
+					"& .MuiDataGrid-cell": {
+						borderBottom: "none",
+					},
+					"& .MuiDataGrid-columnHeaders": {
+						backgroundColor: theme.palette.background.alt,
+						color: theme.palette.secondary[100],
+						borderBottom: "none",
+					},
+					"& .MuiDataGrid-virtualScroller": {
+						backgroundColor: theme.palette.primary.light,
+					},
+					"& .MuiDataGrid-footerContainer": {
+						backgroundColor: theme.palette.background.alt,
+						color: theme.palette.secondary[100],
+						borderTop: "none",
+					},
+					"& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+						color: `${theme.palette.secondary[200]} !important`,
+					},
+				}}
+			>
+				{loading ? (
+					<div>Loading....</div>
+				) : (
+					<div>
+						<div className="header grid grid-cols-5">
+							{/* <div>ID</div> */}
+							<div className="column">N°</div>
+							<div className="column">Nom</div>
+							<div className="column">Portfeuille</div>
+							<div className="column">Score</div>
+							<div className="column">Meilleur score</div>
+						</div>
+						<div>
+							{[...users]
+								.sort(
+									(a, b) => (b.wallet?.amount ?? 0) - (a.wallet?.amount ?? 0)
+								)
+								.map((u, i) => {
+									return (
+										<div className="row grid grid-cols-5" key={u.uid}>
+											<div className="column">{i + 1}</div>
+											<span className="column">
+												{u?.displayName ?? "Nom indisponible"}
+											</span>
 
-
-  return (
-    <Box m="1.5rem 2.5rem">
-      <Header title="Users" subtitle={`Liste des utilisateurs: ${users.length}`} />
-      <Box
-        mt="40px"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: theme.palette.background.alt,
-            color: theme.palette.secondary[100],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: theme.palette.primary.light,
-          },
-          "& .MuiDataGrid-footerContainer": {
-            backgroundColor: theme.palette.background.alt,
-            color: theme.palette.secondary[100],
-            borderTop: "none",
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${theme.palette.secondary[200]} !important`,
-          },
-        }}
-      >
-         {
-          loading ? <div>Loading....</div> : (
-            <div>
-              
-              <div className="row col-4 header" >
-                
-                  {/* <div>ID</div> */}
-                  <div className="column">Nom</div>
-                  <div className="column">Portfeuille</div>
-                  <div className="column">Score</div>
-                  <div className="column">Meilleur score</div>
-              
-              </div>
-              <div>
-                {
-                  users.map((u) => {
-                    return (
-                      <div className="row col-4" key={u.uid}>
-                        {/* <td>
-                          { u.uid }
-                        </td> */}
-                        <span className="column">
-
-                         {u?.displayName ?? "Nom indisponible"}
-                        </span>
-                        
-                        <span className="column">
-                          {u.wallet?.amount ?? 0}
-                        </span>
-                        <span className="column">
-                          { u.score ?? 0}
-                        </span>
-                        <span className="column">
-                          { u.bestScore?.points ?? 0}
-                        </span>
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            </div>
-          )
-        }
-      </Box>
-    </Box>
-  );
+											<span className="column">{u.wallet?.amount ?? 0}</span>
+											<span className="column">{u.score ?? 0}</span>
+											<span className="column">{u.bestScore?.points ?? 0}</span>
+										</div>
+									);
+								})}
+						</div>
+					</div>
+				)}
+			</Box>
+		</Box>
+	);
 };
 
 export default Users;
